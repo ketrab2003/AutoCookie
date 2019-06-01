6a0775ebeac71758e4cb91908e54bcc82d35bc65
ketrab2003 <43708985+ketrab2003@users.noreply.github.com>
2019-06-01 20:23:33 +0200

    Added closing advancements

--- AutoCookie/autocookie.py
+++ AutoCookie/autocookie.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from os import sys
 
 # Settings (Don't touch unless you know what you do!).
 chromedriver = "./chromedriver.exe"
@@ -7,6 +8,11 @@ game_url = "https://orteil.dashnet.org/cookieclicker/"
 cookie_xpath = "//*[@id='bigCookie']"
 upgrades_xpath = "//*[@class='crate upgrade enabled']"
 buildings_xpath = "//*[@class='product unlocked enabled']"
+close_advancements_xpath = "//*[@class='close']"
+
+# Interprete args.
+if '--show-advancements' in sys.argv:
+	close_advancements_xpath = ''
 
 # Initialize chrome and load game from url.
 driver = webdriver.Chrome(chromedriver)
@@ -31,5 +37,13 @@ while running:
 		buildings = driver.find_elements_by_xpath(buildings_xpath)
 		for building in buildings:
 			building.click()
+		
+		# Close advancement messages.
+		closes = driver.find_elements_by_xpath(close_advancements_xpath)
+		for close in closes:
+			close.click()
 	except:
 		pass
+
+# Close website.
+driver.close()
