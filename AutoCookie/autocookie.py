142238e5d6c803e231644a226f93cce36bca0011
ketrab2003 <43708985+ketrab2003@users.noreply.github.com>
2019-06-01 19:57:59 +0200

    Set up
    Setted up v1.0

---
+++ AutoCookie/autocookie.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+
+# Settings (Don't touch unless you know what you do!).
+chromedriver = "./chromedriver.exe"
+game_url = "https://orteil.dashnet.org/cookieclicker/"
+
+cookie_xpath = "//*[@id='bigCookie']"
+upgrades_xpath = "//*[@class='crate upgrade enabled']"
+buildings_xpath = "//*[@class='product unlocked enabled']"
+
+# Initialize chrome and load game from url.
+driver = webdriver.Chrome(chromedriver)
+driver.get(game_url)
+
+# Get cookie object.
+cookie = driver.find_element_by_xpath(cookie_xpath)
+
+running = True
+while running:
+	try:
+		# Click cookie.
+		cookie.click()
+		
+		# Find buyable upgrades and buy them.
+		upgrades = driver.find_elements_by_xpath(upgrades_xpath)
+		for upgrade in upgrades:
+			upgrade.click()
+		
+		# Same for buildings.
+		buildings = driver.find_elements_by_xpath(buildings_xpath)
+		for building in buildings:
+			building.click()
+	except:
+		pass
